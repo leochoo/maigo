6b3001d5fb62091eb97a726b1dc3c615d23296e1
Doyoon Lee <dynleedev@gmail.com>
2021-12-23 17:58:44 +0900

    Fixed timer error

--- src/components/Game/BeforeGame.svelte
+++ src/components/Game/BeforeGame.svelte
@@ -1,41 +1,49 @@
 <script lang="ts">
   import { db } from "../../../firebase";
-  import { getContext, onDestroy } from "svelte";
-  import {
-    doc,
-    onSnapshot,
-    updateDoc,
-    serverTimestamp,
-  } from "firebase/firestore";
+  import { getContext } from 'svelte';
+  import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+  import { currTimeUTC } from '../../store';
   export let room_id: string;
+  let data: any = [];
+  let userList = [];
 
   let updateGamePhase:()=>void = getContext('updateGamePhase');
-  let game_start = false;
 
-  const addEndTime = async () => {
-    const docRef = doc(db, "rooms", room_id);
-    // put the current time for development purpose
-    const currentServerTime = serverTimestamp();
-    console.log("serverTime: ", currentServerTime);
-    await updateDoc(docRef, {
-      endTime: currentServerTime,
-    });
-    console.log("update done");
-  };
-
-  // detach onSnapshot
-  onDestroy(() => {
-    console.log("BeforeGame destroyed");
-    // unsub();
+  // consider firestore latency compensation
+  const unsub = onSnapshot(doc(db, "rooms", room_id),{includeMetadataChanges: false}, (doc) => {
+    const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+    console.log(source, " Current room data: ", doc.data());
+    data = doc.data();
+    userList = data.users;
+    console.log("userlist: ", userList);
   });
+  
+  const addEndTime = async () => {
+    const docRef = doc(db,"rooms",room_id);
+    var endTime: Date;
+    // Set game time limits here
+    const timeLimits:number = 5 
+    // get the current time and add time limits to it. And add the calculated endTime in firestore
+    const _currUTCTime = await fetch("http://worldtimeapi.org/api/timezone/Etc/UTC");
+    const _data = await _currUTCTime.json();
+    currTimeUTC.set(_data.datetime);
+    endTime = new Date(new Date($currTimeUTC).getTime()+timeLimits*60000);
+    await updateDoc(docRef,{
+        endTime: endTime
+      })
+  }
 </script>
-
 <template>
   <p>Before Game</p>
-  <button
-    on:click={async () => {
-      await addEndTime();
-      updateGamePhase();
-    }}>Start game</button
-  >
+  <h2>Room ID: {room_id}</h2>
+  <ul>
+    <h2>Users</h2>
+    {#each userList as user}
+      <li>{user}</li>
+    {/each}
+  </ul>
+  <button on:click={async () =>{
+    await addEndTime();
+    updateGamePhase();
+  }}>Start game</button>
 </template>
