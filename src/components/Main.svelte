d4061be4810a4d79c09e627d10937dcd3022e040
kMed <kenta.medina@protonmail.com>
2022-01-13 00:32:45 -0800

    fixed glass effect

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -112,12 +112,12 @@
     margin: 0 auto;
 
     /* add glass effect */
-		/* background: rgba( 255, 255, 255, 0.35 );
+		background: rgba( 255, 255, 255, 0.35 );
 		box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-		backdrop-filter: blur( 4.5px );
+		/* backdrop-filter: blur( 4.5px ); */
 		-webkit-backdrop-filter: blur( 4.5px );
 		border-radius: 10px;
-		border: 1px solid rgba( 255, 255, 255, 0.18 ); */
+		border: 1px solid rgba( 255, 255, 255, 0.18 );
   }
 
   img {
