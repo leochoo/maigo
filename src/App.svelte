44f24a1a5cae479b390ca25985d25670958e95c0
leochoo <leochoodev@gmail.com>
2022-02-02 14:50:14 +0900

    updated to match dev

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,92 @@
 <script lang="ts">
+  import { onMount, setContext, getContext } from "svelte";
+  import { getAuth, onAuthStateChanged } from "firebase/auth";
+  import { Loader } from "@googlemaps/js-api-loader";
   import logo from "./assets/svelte.png";
-  import Counter from "./lib/Counter.svelte";
+  import Main from "./components/Main.svelte";
+  import Auth from "./components/Auth.svelte";
+  import { currentUser } from "./store";
+  import { Router, Link, Route } from "svelte-routing";
+
+  const loader = new Loader({
+    // Don't use this api key below unless you need it
+    // apiKey: import.meta.env.VITE_MAPS_JS_API as string,
+    apiKey: "",
+    language: "ja",
+  });
+  setContext("loader", loader);
+  const auth = getAuth();
+  onMount(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        currentUser.set({
+          isLoggedIn: true,
+          user: user,
+        });
+      } else {
+        currentUser.set({
+          isLoggedIn: false,
+          user: null,
+        });
+      }
+    });
+  });
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>MaiGO</h1>
+<link rel="stylesheet" href="src/styles.css" />
 
-  <Counter />
-
-  <p>A GeoGeussr Clone for Multiplayer Online</p>
-
-  <p>HHLAB is a group of students from Keio University, Japan.</p>
-</main>
+<Router>
+  <main>
+    <div class="main">
+      {#if $currentUser.isLoggedIn}
+        <Main />
+      {:else}
+        <img src={logo} alt="Svelte Logo" />
+        <h1>MaiGO</h1>
+        <Auth />
+      {/if}
+    </div>
+    <!-- <Route path="upload" component={Upload} /> -->
+    <!-- <Route path="charts" component={Charts} /> -->
+    <!-- <Route path="login" component={Login} /> -->
+  </main>
+</Router>
 
 <style>
   :root {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  }
 
-  main {
+    /* Fullscreen background code */
+    background: url(src/assets/earthFromSpace.jpeg) no-repeat center center
+      fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+
+    /*scrolling background code */
+    /* background: url("src/assets/panoramic_background.jpeg") no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover; */
+    /* animation: slide 60s linear infinite; */
+  }
+  .main {
+    /* display: flex; */
+    flex-direction: column;
     text-align: center;
     padding: 1em;
     margin: 0 auto;
+    padding-top: 3em;
   }
-
   img {
+    position: relative;
+    left: calc(50% - 8rem);
     height: 16rem;
     width: 16rem;
   }
-
   h1 {
     color: #ff3e00;
     text-transform: uppercase;
@@ -41,19 +97,12 @@
     max-width: 14rem;
   }
 
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
+  @keyframes slide {
+    0% {
+      transform: translate3d(0, 0, 0);
     }
-
-    p {
-      max-width: none;
+    100% {
+      transform: translate3d(-1692px, 0, 0);
     }
   }
 </style>
