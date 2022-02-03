151a5f77a007ed2d5c923af0a3f4d29dd822ba3a
kMed <kenta.medina@protonmail.com>
2022-02-02 18:18:31 -0800

    formatting timer

--- src/components/Game/Timer.svelte
+++ src/components/Game/Timer.svelte
@@ -68,13 +68,22 @@
 	.timer {
 		/* position: absolute; */
 		background-color: black;
-		width:18%;
-		height: 10%;
+		width:3em;
+		height: 1.2em;
 		z-index: 0;
 		font-size: 4rem;
-		text-align: left;
+		text-align: center;
 		margin-left: -80%;
 		margin-top: -2%;
-		color:#F32500;
+		/* color:#F32500; */
+		color:red;
+
+		/* add glass effect */
+		background: rgba(0,0,0, 0.55 );
+		box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+		backdrop-filter: blur( 4.5px );
+		-webkit-backdrop-filter: blur( 4.5px );
+		border-radius: 10px;
+		border: 1px solid rgba( 255, 255, 255, 0.18 );
 	}
 </style>
