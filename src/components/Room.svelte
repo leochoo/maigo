d9b0e61471c6e31b2ef28d8e51929c586f4a02af
leochoo <leochoodev@gmail.com>
2022-02-03 11:19:04 +0900

    fixed replay button issue

--- src/components/Room.svelte
+++ src/components/Room.svelte
@@ -13,7 +13,7 @@
   import BeforeGame from "./Game/BeforeGame.svelte";
   import AfterSubmit from "./Game/AfterSubmit.svelte";
   import DuringGame from "./Game/DuringGame.svelte";
-  import { room_available } from '../store.js'
+  import { room_available } from "../store.js";
 
   export let room_id: string;
   let gamePhase: number;
@@ -25,12 +25,19 @@
   let leaveCount: number = 0;
   let replayCount: number = 0;
   let buttonClicked = false;
-  
+
+  onMount(() => {
+    console.log("onMount");
+    console.log("buttonClicked", buttonClicked);
+  });
+
   $: if (gamePhase == 1 && leaveCount + replayCount == userUidList.length) {
+    console.log("checking gamePhase", buttonClicked);
+
+    buttonClicked = false;
     if (leaveCount >= replayCount) {
       deleteRoom();
-    }
-    else {
+    } else {
       //initialization of the room
       initRoom();
     }
@@ -44,29 +51,32 @@
   }
   setContext("updateGamePhase", updateGamePhase);
 
-  const userLeaveRoom = async() => {
+  const userLeaveRoom = async () => {
+    buttonClicked = true;
     const docRef = doc(db, "rooms", room_id);
     await updateDoc(docRef, {
-      leave_count: increment(1)
+      leave_count: increment(1),
     });
-    buttonClicked = true;
-  }
+  };
 
   const userReplay = async () => {
+    buttonClicked = true;
     const docRef = doc(db, "rooms", room_id);
     await updateDoc(docRef, {
-      replay_count: increment(1)
+      replay_count: increment(1),
     });
-    buttonClicked = true;
-  }
+  };
 
   const deleteRoom = async () => {
+    console.log("deleteRoom buttonClicked", buttonClicked);
+
     const docRef = doc(db, "rooms", room_id);
     await deleteDoc(docRef);
     room_available.set(false);
-  }
+  };
 
   const initRoom = async () => {
+    console.log("initRoom buttonClicked", buttonClicked);
     const docRef = doc(db, "rooms", room_id);
     await updateDoc(docRef, {
       gamePhase: 0,
@@ -76,78 +86,86 @@
       submit_count: 0,
       endTime: 0,
       startTime: 0,
-    })
-  }
+    });
+    // buttonClicked = false;
+  };
 
   // consider firestore latency compensation
-  const unsub = onSnapshot(
-    doc(db, "rooms", room_id),
-    (roomRef) => {
-      const source = roomRef.metadata.hasPendingWrites ? "Local" : "Server";
-      let _userInfoList = [];
-      //console.log(source, " Current room data: ", roomRef.data());
-      data = roomRef.data();
-      userUidList = data.users;
-      gamePhase = data.gamePhase;
-      submitCount = data.submit_count;
-      leaveCount = data.leave_count;
-      replayCount = data.replay_count;
-      // get user data from userUidList using getDoc and push to userInfoList
-      userUidList.forEach((userUid) => {
-        const userRef = doc(db, "users", userUid);
-        getDoc(userRef).then((userDoc) => {
+  const unsub = onSnapshot(doc(db, "rooms", room_id), (roomRef) => {
+    const source = roomRef.metadata.hasPendingWrites ? "Local" : "Server";
+    let _userInfoList = [];
+    //console.log(source, " Current room data: ", roomRef.data());
+    data = roomRef.data();
+    userUidList = data.users;
+    gamePhase = data.gamePhase;
+    submitCount = data.submit_count;
+    leaveCount = data.leave_count;
+    replayCount = data.replay_count;
+    // get user data from userUidList using getDoc and push to userInfoList
+    userUidList.forEach((userUid) => {
+      const userRef = doc(db, "users", userUid);
+      getDoc(userRef)
+        .then((userDoc) => {
           _userInfoList = [..._userInfoList, userDoc.data()];
-        }).then(()=> {
+        })
+        .then(() => {
           userInfoList = _userInfoList;
           isLoading = false;
         });
-      });
-    }
-  );
-  onDestroy(()=>unsub);
+    });
+  });
+  onDestroy(() => unsub);
 </script>
 
-{#if gamePhase==0}
+{#if gamePhase == 0}
   <div class="glasseffect">
     <h2>Room ID: {room_id}</h2>
-      <h3>Current Users:</h3>
+    <h3>Current Users:</h3>
     <ul>
       {#each userInfoList as user}
         <li>
-          <img src={user.photoURL} alt="" style="width:2em; height:2em"/>
-          <br>
+          <img src={user.photoURL} alt="" style="width:2em; height:2em" />
+          <br />
           {user.displayName}
         </li>
       {/each}
     </ul>
     {#if !isLoading}
       <Chat {room_id} />
-      <BeforeGame {room_id}/>
+      <BeforeGame {room_id} />
     {:else}
       Loading...
     {/if}
   </div>
-<!-- Enter Result Page when submit counts equal to the number of users in the room -->
+  <!-- Enter Result Page when submit counts equal to the number of users in the room -->
 {:else if submitCount == userUidList.length}
   <div class="glasseffect">
-    <AfterSubmit {room_id} {userInfoList}/>
-  <Chat {room_id} />
-  <span style="color: whitesmoke;">Leave Count: {leaveCount}</span>
-  <span style="color: whitesmoke;">Replay Count: {replayCount}</span>
-  <button on:click={async () =>{
-    await userLeaveRoom();
-  }} disabled={buttonClicked}>
-    Leave the Room
-  </button>
-  <button on:click={async ()=>{
-    await userReplay();
-  }} disabled={buttonClicked}>
-    Replay
-  </button>
+    <AfterSubmit {room_id} {userInfoList} />
+    <Chat {room_id} />
+    <span style="color: whitesmoke;">Leave Count: {leaveCount}</span>
+    <span style="color: whitesmoke;">Replay Count: {replayCount}</span>
+    <button
+      on:click={async () => {
+        await userLeaveRoom();
+        console.log("onleaveclick", buttonClicked);
+      }}
+      disabled={buttonClicked}
+    >
+      Leave the Room
+    </button>
+    <button
+      on:click={async () => {
+        await userReplay();
+        console.log("onreplayclick", buttonClicked);
+      }}
+      disabled={buttonClicked}
+    >
+      Replay
+    </button>
   </div>
 {:else if gamePhase == 1}
   <!-- <Chat {room_id} /> -->
-  <DuringGame {room_id}/>
+  <DuringGame {room_id} />
 {/if}
 
 <style>
@@ -158,16 +176,16 @@
     text-align: center;
     padding: 1em;
     margin: 0 auto;
-    width:30em;
+    width: 30em;
     /* height:40em; */
     color: white;
     /* add glass effect */
-    background: rgba( 255, 255, 255, 0.15 );
-    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-    backdrop-filter: blur( 4.5px );
-    -webkit-backdrop-filter: blur( 4.5px );
+    background: rgba(255, 255, 255, 0.15);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(4.5px);
+    -webkit-backdrop-filter: blur(4.5px);
     border-radius: 10px;
-    border: 1px solid rgba( 255, 255, 255, 0.18 );
+    border: 1px solid rgba(255, 255, 255, 0.18);
   }
   button {
     z-index: 1;
@@ -188,14 +206,13 @@
   button:active {
     background: grey;
   }
-  ul{
-    padding:0;
-    list-style-type:none;
+  ul {
+    padding: 0;
+    list-style-type: none;
   }
 
-  li{
-      float:left;
-      padding: 0.5em;
+  li {
+    float: left;
+    padding: 0.5em;
   }
-
 </style>
